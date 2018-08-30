571911fded9d8ac841e4f699666f8becfdc14f00
Pei Wu <peiwu@peidemacbook-air.local>
2018-08-30 09:44:18 +0800

    LectureVII, Homework #12 BurgerQueen

--- LectureVII_ColorDice/ViewController.swift
+++ LectureVII_ColorDice/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewColor: UIImageView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var alphaLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func colorSliderChange(_ sender: UISlider) {
+        imageViewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    
+        var numberString = String(format:"%.2f", sender.value)
+        
+        if sender == redSlider {
+            redLabel.text = numberString
+        } else if sender == blueSlider {
+            blueLabel.text = numberString
+        } else if sender == greenSlider {
+            greenLabel.text = numberString
+        } else if sender == alphaSlider {
+            alphaLabel.text = numberString
+        }
+    
+    }
+    
+    @IBAction func randomChange(_ sender: Any) {
+        let randomRed = CGFloat.random(in: 0...1)
+        let randomGreen = CGFloat.random(in: 0...1)
+        let randomBlue = CGFloat.random(in: 0...1)
+        let randomAlpha = CGFloat.random(in: 0...1)
+        
+        imageViewColor.backgroundColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: randomAlpha)
+        
+        redSlider.value = Float(randomRed)
+        redLabel.text = String(format:"%.2f", randomRed)
+        greenSlider.value = Float(randomGreen)
+            greenLabel.text = String(format:"%.2f", randomGreen)
+        blueSlider.value = Float(randomBlue)
+            blueLabel.text = String(format:"%.2f", randomBlue)
+        alphaSlider.value = Float(randomAlpha)
+            alphaLabel.text = String(format:"%.2f", randomAlpha)
+        
+        
+    }
+    
+    
+    
 }
+
 
